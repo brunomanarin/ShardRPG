34138871215ab3e2ffd6006bd6cbf42f48eecb4a
Huan <huan150@hotmail.com>
2019-05-18 13:16:03 -0300

    Game class add

--- br.ufsc.ine5605.ShardRPG/src/br/ufsc/ine5605/ShardRPG/Control/Game.java
+++ br.ufsc.ine5605.ShardRPG/src/br/ufsc/ine5605/ShardRPG/Control/Game.java
@@ -14,39 +14,61 @@ public class Game {
 
 	Player player;
 
+	RegisterPlayerHandler playerHandler;
+
 
 	public Game() throws Exception {
-		scanner = new Scanner(System.in);
-		jsonHandler = new JsonHandler();
-		Map<String, Player> mapList;
-		int input;
-		System.out.println("Digite 1 para começar um NOVO JOGO ou 2 para CARREGAR um jogo:");
-		do {
-			System.out.println("Digite um numero válido!");
-			System.out.print("> ");
-			input = scanner.nextInt();
-			scanner.nextLine();
-		} while (input != 1 && input != 2);
+		try {
+			playerHandler = new RegisterPlayerHandler();
+			scanner = new Scanner(System.in);
+			jsonHandler = new JsonHandler();
+			Map<String, Player> mapList;
+			int input;
+			System.out.println("Digite 1 para começar um NOVO JOGO ou 2 para CARREGAR um jogo:");
+			do {
+				System.out.println("Digite um numero válido!");
+				System.out.print("> ");
+				input = scanner.nextInt();
+				scanner.nextLine();
+			} while (input != 1 && input != 2);
 
-		if (input == 1) {
-			System.out.println(jsonHandler.registerPlayer(new RegisterPlayerHandler().registerNewPlayer()));
-		} else {
-			String key;
-			final String playersList = jsonHandler.playerListing();
-			final File file = new File("PlayersList.json");
-			if (playersList == null || file.exists()) {
-				System.out.println("Não há jogos carregados!  Começaremos uma nova aventura: \n");
-				jsonHandler.registerPlayer(new RegisterPlayerHandler().registerNewPlayer());
-				file.delete();
+			if (input == 1) {
+				player = playerHandler.registerNewPlayer();
+				System.out.println(jsonHandler.registerPlayer(player));
+
 			} else {
-				System.out.println(playersList);
-				do {
-					System.out.println("Escolha uma chave: ");
-					System.out.print("> ");
-					key = scanner.nextLine().toUpperCase();
-					mapList = jsonHandler.allPlayers();
-				} while (!mapList.containsKey(key));
+				String key;
+				final String playersList = jsonHandler.playerListing();
+				final File file = new File("PlayersList.json");
+				if (playersList == null || !file.exists()) {
+					System.out.println("Não há jogos carregados!  Começaremos uma nova aventura: \n");
+					file.delete();
+					player = playerHandler.registerNewPlayer();
+					System.out.println(jsonHandler.registerPlayer(player));
+
+				} else {
+					System.out.println(playersList);
+					do {
+						System.out.println("Escolha uma chave: ");
+						System.out.print("> ");
+						key = scanner.nextLine().toUpperCase();
+						mapList = jsonHandler.allPlayers();
+					} while (!mapList.containsKey(key));
+					player = mapList.get(key);
+					System.out.println("Login efetuado com sucesso!");
+				}
 			}
+		} catch (final Exception e) {
+			System.out.println(e);
+		}
+	}
+
+
+	public void start() {
+		try {
+
+		} catch (final Exception e) {
+			// TODO: handle exception
 		}
 	}
 
--- br.ufsc.ine5605.ShardRPG/src/br/ufsc/ine5605/ShardRPG/Info/Room.java
+++ br.ufsc.ine5605.ShardRPG/src/br/ufsc/ine5605/ShardRPG/Info/Room.java
@@ -1,54 +1,68 @@
 package br.ufsc.ine5605.ShardRPG.Info;
 
 public class Room {
+
 	private String name;
+
 	private String description;
+
 	private String descriptionAfter;
+
 	private boolean wasVisited;
-	private Item objects;
-	public Room(String name, String descriptionFirstVisit, String descriptionAfter, Item objects) {
+
+
+	// private Item objects;
+	public Room(String name, String descriptionFirstVisit, String descriptionAfter) {
 		this.name = name;
-		this.description = descriptionFirstVisit;
+		description = descriptionFirstVisit;
 		this.descriptionAfter = descriptionAfter;
-		this.objects = objects;
+		// this.objects = objects;
 	}
+
+
 	public String getName() {
 		return name;
 	}
+
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+
 	public String getDescription() {
 		return description;
 	}
+
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
+
 	public String getDescriptionAfter() {
 		return descriptionAfter;
 	}
+
+
 	public void setDescriptionAfter(String descriptionAfter) {
 		this.descriptionAfter = descriptionAfter;
 	}
+
+
 	public boolean getWasVisited() {
 		return wasVisited;
 	}
+
+
 	public void setWasVisited(boolean wasVisited) {
 		this.wasVisited = wasVisited;
 	}
-	
+
+
 	public void checkPlayerFirstVisit() {
-		if(!wasVisited) {
+		if (!wasVisited) {
 			wasVisited = true;
-		}	
-	}
-	public String showDescription() {
-		return this.wasVisited ? this.descriptionAfter : this.description + visibleObjects();
-	}
-	public String visibleObjects() {
-		String visibleObjects = "";
-		for(Item object: this.objects) {
-			visibleObjects+= "There is a " + object.itemDescription() + "here \n";
 		}
 	}
 
